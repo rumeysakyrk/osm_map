9f4a2ecdae7478f1f31b97ce289718d954852ee2
rmysk <rumeysakyrk@gmail.com>
2022-08-09 16:12:31 +0300

    find rotate and some info

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,10 +1,10 @@
+import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 import 'dart:math';
-
 import 'main.dart';
 
 class MainPage extends StatefulWidget {
@@ -18,6 +18,55 @@ class _MainPageState extends State<MainPage> {
   double value = 0;
   late MapController controller;
   ValueNotifier<bool> trackingNotifier = ValueNotifier(false);
+  ValueNotifier<bool> showFab = ValueNotifier(true);
+  late GlobalKey<ScaffoldState> scaffoldKey;
+
+  late TextEditingController textEditingController = TextEditingController();
+  ValueNotifier<GeoPoint?> notifierGeoPoint = ValueNotifier(null);
+  ValueNotifier<bool> notifierAutoCompletion = ValueNotifier(false);
+  late StreamController<List<SearchInfo>> streamSuggestion = StreamController();
+  late Future<List<SearchInfo>> _futureSuggestionAddress;
+  String oldText = "";
+  Timer? _timerToStartSuggestionReq;
+  final Key streamKey = const Key("streamAddressSug");
+
+  Future reInitStream() async {
+    notifierAutoCompletion.value = false;
+    await streamSuggestion.close();
+    setState(() {
+      streamSuggestion = StreamController();
+    });
+  }
+
+  Future<void> suggestionProcessing(String addr) async {
+    notifierAutoCompletion.value = true;
+    _futureSuggestionAddress = addressSuggestion(
+      addr,
+      limitInformation: 5,
+    );
+    _futureSuggestionAddress.then((value) {
+      streamSuggestion.sink.add(value);
+    });
+  }
+
+  void textOnChanged() async {
+    final v = textEditingController.text;
+    if (v.length > 3 && oldText != v) {
+      oldText = v;
+      if (_timerToStartSuggestionReq != null &&
+          _timerToStartSuggestionReq!.isActive) {
+        _timerToStartSuggestionReq!.cancel();
+      }
+      _timerToStartSuggestionReq =
+          Timer.periodic(const Duration(seconds: 2), (timer) async {
+        await suggestionProcessing(v);
+        timer.cancel();
+      });
+    }
+    if (v.isEmpty) {
+      await reInitStream();
+    }
+  }
 
   Future<void> _signOut() async {
     await FirebaseAuth.instance.signOut();
@@ -25,34 +74,120 @@ class _MainPageState extends State<MainPage> {
 
   @override
   void initState() {
+    Km = "-";
+    duration = "-";
+    textEditingController.addListener(textOnChanged);
+    value = 0;
     super.initState();
     controller = MapController(
       initMapWithUserPosition: true,
       initPosition: GeoPoint(
-        latitude: 47.4358055,
-        longitude: 8.4737324,
+        latitude: 42.4358055,
+        longitude: 40.4737324,
       ),
     );
   }
 
   @override
+  void dispose() {
+    textEditingController.removeListener(textOnChanged);
+    super.dispose();
+  }
+
+  void roadActionBt(GeoPoint point1, GeoPoint point2, RoadType type) async {
+    try {
+      await controller.removeLastRoad();
+      showFab.value = true;
+
+      RoadInfo roadInformation = await controller.drawRoad(
+        point1,
+        point2,
+        roadType: type,
+        roadOption: const RoadOption(
+          roadWidth: 10,
+          roadColor: Colors.blue,
+          showMarkerOfPOI: true,
+          zoomInto: true,
+        ),
+      );
+      setState(() {
+        duration = Duration(seconds: roadInformation.duration!.toInt())
+            .inMinutes
+            .toString();
+        Km = roadInformation.distance!.floorToDouble().toString();
+      });
+
+      print(
+          "duration:${Duration(seconds: roadInformation.duration!.toInt()).inHours}");
+      print("distance:${roadInformation.distance}Km");
+      print(roadInformation.route.length);
+    } on RoadException catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            "${e.errorMessage()}",
+          ),
+        ),
+      );
+    }
+  }
+
+  int count = 0;
+  late String duration = "-", Km = "-";
+  GeoPoint point1 = GeoPoint(latitude: 42, longitude: 40);
+  GeoPoint point2 = GeoPoint(latitude: 42, longitude: 40);
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 150.0,
+        toolbarHeight: 130.0,
         backgroundColor: Colors.teal,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(
-              child: TextButton(onPressed: (){
-                setState((){
-                  value=(value+1)%2;
-                });
-              }, child: Icon(Icons.menu_outlined,size: 30,color: Colors.white,),),
+        actions: [
+          Flexible(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () { setState(() {
+                        value = (value + 1) % 2;
+                      });},
+                      icon: const  Icon(
+                        Icons.menu_outlined,
+                        size: 30,
+                        color: Colors.white,
+                      ),
+                    ),
+
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    IconButton(
+                      onPressed: () => roadActionBt(point1, point2, RoadType.car),
+                      icon: const Icon(Icons.car_crash),
+                    ),
+                    IconButton(
+                        onPressed: () =>
+                            roadActionBt(point1, point2, RoadType.bike),
+                        icon: const Icon(Icons.pedal_bike)),
+                    IconButton(
+                      onPressed: () =>
+                          roadActionBt(point1, point2, RoadType.foot),
+                      icon: const Icon(Icons.directions_walk),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Text("Süre: " + duration + "  ", style: TextStyle(fontSize: 16),),SizedBox(width: 25,), Text("Km: " + Km, style: TextStyle(fontSize: 16))],
+                )
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
       body: Stack(children: [
         Container(
@@ -71,7 +206,6 @@ class _MainPageState extends State<MainPage> {
             children: [
               DrawerHeader(
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     CircleAvatar(
                         radius: (52),
@@ -85,37 +219,116 @@ class _MainPageState extends State<MainPage> {
                     ),
                     Text(
                       "TravelApp",
-                      style: TextStyle(fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold),
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
                     )
                   ],
                 ),
               ),
-              Expanded(
-                  child: ListView(
-                children: [
-                  ListTile(
-                    onTap: () {},
-                    leading: Icon(
-                      Icons.home,
-                      color: Colors.black,
+              TextField(
+                controller: textEditingController,
+                onEditingComplete: () async {
+                  FocusScope.of(context).requestFocus(new FocusNode());
+                },
+                decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.black,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25),
+                    borderSide: BorderSide(
+                      width: 0,
+                      style: BorderStyle.none,
                     ),
-                    title: Text("Ana Sayfa",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500),
-                  )
-                  )],
-              )),
-              Expanded(
-                  child: ListView(
-                children: [
-                  ListTile(
-                    onTap: () {},
-                    leading: Icon(
-                      Icons.settings,
-                      color: Colors.black,
+                  ),
+                  filled: true,
+                  fillColor: Colors.teal.shade200,
+                  contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 0),
+                  suffix: ValueListenableBuilder<TextEditingValue>(
+                    valueListenable: textEditingController,
+                    builder: (ctx, text, child) {
+                      if (text.text.isNotEmpty) {
+                        return child!;
+                      }
+                      return SizedBox.shrink();
+                    },
+                    child: InkWell(
+                      focusNode: FocusNode(),
+                      onTap: () {
+                        textEditingController.clear();
+                        FocusScope.of(context)
+                            .requestFocus(new FocusNode());
+                      },
+                      child: Icon(
+                        Icons.close,
+                        size: 16,
+                        color: Colors.black,
+                      ),
                     ),
-                    title: Text("Ayarlar",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500),),
-                  )
-                ],
-              )),
+                  ),
+                  focusColor: Colors.black,
+                  hintText: "Ara",
+                ),
+              ),
+              SizedBox(height: 20,),
+              Expanded(
+                child: StreamBuilder<List<SearchInfo>>(
+                  stream: streamSuggestion.stream,
+                  key: streamKey,
+                  builder: (ctx, snap) {
+                    if (snap.hasData) {
+                      return ListView.builder(
+                        itemExtent: 50.0,
+                        itemBuilder: (ctx, index) {
+                          return ListTile(
+                            title: Text(
+                              snap.data![index].address.toString(),
+                              maxLines: 1,
+                              overflow: TextOverflow.fade,
+                            ),
+                            onTap: () async {
+                              if (count < 2) {
+                                if (count == 1) {
+                                  point1 = snap.data![index].point!;
+                                } else {
+                                  point2 = snap.data![index].point!;
+                                }
+                                count++;
+                                controller
+                                    .addMarker(snap.data![index].point!);
+                                controller.goToLocation(
+                                  snap.data![index].point!,
+                                );
+                                controller.setZoom(zoomLevel: 20);
+
+                                /// hide suggestion card
+                                notifierAutoCompletion.value = false;
+                                await reInitStream();
+                                FocusScope.of(context).requestFocus(
+                                  new FocusNode(),
+                                );
+                              }
+                            },
+                          );
+                        },
+                        itemCount: snap.data!.length,
+                      );
+                    }
+                    if (snap.connectionState == ConnectionState.waiting) {
+                      return Card(
+                        child: Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      );
+                    }
+                    return SizedBox();
+                  }
+                ),
+              ),
+              SizedBox(height: 20,),
               Expanded(
                   child: ListView(
                 children: [
@@ -125,23 +338,27 @@ class _MainPageState extends State<MainPage> {
                         Icons.logout,
                         color: Colors.black,
                       ),
-                      title: TextButton(child:Text("Çıkış",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500)),onPressed: (){
-                        _signOut();
-                        Navigator.pushAndRemoveUntil(context, MaterialPageRoute(
-                          builder: (context) {
-                            return HomePage();
-                          },
-                        ), (route) => false);
-                      },
-                      )
-                  )
+                      title: TextButton(
+                        child: Text("Çıkış",
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.w500,color:Colors.black)),
+                        onPressed: () {
+                          _signOut();
+                          Navigator.pushAndRemoveUntil(context,
+                              MaterialPageRoute(
+                            builder: (context) {
+                              return HomePage();
+                            },
+                          ), (route) => false);
+                        },
+                      ))
                 ],
               ))
             ],
           ),
         )),
         TweenAnimationBuilder(
-          curve: Curves.easeIn,
+            curve: Curves.easeIn,
             tween: Tween<double>(begin: 0, end: value),
             duration: Duration(milliseconds: 500),
             builder: (_, double val, __) {
@@ -152,8 +369,14 @@ class _MainPageState extends State<MainPage> {
                   ..setEntry(0, 3, 200 * val)
                   ..rotateY((pi / 6) * val),
                 child: Scaffold(
-                  body: OSMFlutter(
-                    controller: controller,
+                  body: Stack(
+                    children: [
+                      Expanded(
+                        child: OSMFlutter(
+                          controller: controller,
+                        ),
+                      ),
+                    ],
                   ),
                   floatingActionButton: FloatingActionButton(
                     backgroundColor: Colors.teal,
@@ -161,7 +384,6 @@ class _MainPageState extends State<MainPage> {
                       if (!trackingNotifier.value) {
                         await controller.currentLocation();
                         await controller.enableTracking();
-                        //await controller.zoom(5.0);
                       } else {
                         await controller.disabledTracking();
                       }
@@ -171,9 +393,9 @@ class _MainPageState extends State<MainPage> {
                       valueListenable: trackingNotifier,
                       builder: (ctx, isTracking, _) {
                         if (isTracking) {
-                          return Icon(Icons.gps_off_sharp);
+                          return const Icon(Icons.gps_off_sharp);
                         }
-                        return Icon(Icons.my_location);
+                        return const Icon(Icons.my_location);
                       },
                     ),
                   ),
